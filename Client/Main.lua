e9ef44bb960303f77185802e877e07c31defebb2
Olvidate <82069828+Olvidate@users.noreply.github.com>
2021-07-26 18:15:32 -0400

    deleted ESX deprecated method

--- Client/Main.lua
+++ Client/Main.lua
@@ -1,13 +1,5 @@
-ESX = nil
 local playerLoaded = false
 local firstSpawn = true
-
-Citizen.CreateThread(function()
-  while ESX == nil do
-      TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-      Citizen.Wait(0)
-  end
-end)
 
 function openSkinMenu()
     local config = {
